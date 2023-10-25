3fc81fe36103450a3be3bd7450d59b478bb192e1
brunofbpaula <deepcooper@outlook.com>
2023-10-25 05:19:53 -0300

    Rename class

---
+++ src/main/java/com/blackout/main/TheArrays.java
@@ -0,0 +1,18 @@
+package com.blackout.main;
+
+public class TheArrays {
+
+	public static void main(String[] args) {
+		
+		
+		/* Basically a list of cells
+		 * Fixed in size
+		 * Fast for data retrieval
+		 * Compact memory usage if size is known
+		 * Delete operation very hard */
+		
+		
+
+	}
+
+}
