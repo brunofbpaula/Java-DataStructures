32fa481fe99823fa8e2589df8c19a90e3e377a05
brunofbpaula <deepcooper@outlook.com>
2023-10-25 05:39:00 -0300

    Add array examples

--- src/main/java/com/blackout/main/TheArrays.java
+++ src/main/java/com/blackout/main/TheArrays.java
@@ -1,5 +1,8 @@
 package com.blackout.main;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class TheArrays {
 
 	public static void main(String[] args) {
@@ -11,8 +14,21 @@ public class TheArrays {
 		 * Compact memory usage if size is known
 		 * Delete operation very hard */
 		
+		String[] colors = new String[5];
+		colors[0] = "Black";
+		colors[1] = "Red";
 		
-
+		// Printing it out 
+		// We need to convert the array to string, if we want to print it entirely
+		System.out.println(Arrays.toString(colors));
+		
+		int[] numbers = {100, 200};
+		
+		// Printing the numbers between 100 and 200
+		for (int counter = numbers[0]; counter < numbers[1]; counter ++) {
+			System.out.println(counter);
+		}
+		
 	}
 
 }
