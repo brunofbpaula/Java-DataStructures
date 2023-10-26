2345968149d6841a97546711895a9a40ed958da3
brunofbpaula <deepcooper@outlook.com>
2023-10-26 00:13:59 -0300

    Add LinkedList explanation

--- src/main/java/com/blackout/main/TheLists.java
+++ src/main/java/com/blackout/main/TheLists.java
@@ -1,6 +1,7 @@
 package com.blackout.main;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class TheLists {
@@ -25,6 +26,32 @@ public class TheLists {
         System.out.println(colors.size());
         // Contains method (returns a boolean)
         System.out.println(colors.contains("Black"));
+
+        // Using of method (Unmodifiable)
+        List<String> myfriends = List.of(
+                "Lipe",
+                "Miguel"
+        );
+        System.out.println(myfriends);
+
+        // LinkedList
+        // At first sight, it's the same as an ArrayList
+        // Behind the scenes, they work pretty different
+        // A linked list is not a collection like an array
+        // Its element as only connected to their previous or next ones
+        // For example, the second element is linked to the first and third
+        // When we want to retrieve one specific value, if we give an index, it will need to
+        // iterate through all the previous elements of the wanted one. It either starts from the first or the last element.
+        // But, when we need to remove an element, it's way more effective. Instead of creating a new whole array that can contain
+        // All elements, it will simply disconnect two links and rewire them. No need to get the rest of the list evolved.
+        // Same to adding values.
+
+        LinkedList<String> myoldfriends = new LinkedList<>();
+        myoldfriends.add("Beatriz");
+        myoldfriends.add("Alan");
+        myoldfriends.add("Guilherme");
+
+        System.out.println(myoldfriends);
 
     }
 
